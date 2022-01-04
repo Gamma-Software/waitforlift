8c215b307bbd016150a245a0ee0cae1eda9aa33a
Rudloff <valentin.rudloff@navya.tech>
2022-01-04 23:57:05 +0100

    test with the eval command

--- test.sh
+++ test.sh
@@ -1,2 +1,15 @@
 #!/bin/bash
-echo $!
+eval $1 &
+
+cmdpid=$! # Get the pid from the command
+music=$(cat /usr/share/waitforlift/waitforlift.conf)
+mpv $music --loop &> /dev/null &> /dev/null
+while [ 1 ]
+do
+    ps -p $cmdpid > /dev/null
+    if [[ $? -ne 0 ]]; then
+        pkill -9 mpv &> /dev/null
+        break
+    fi
+    sleep 1
+done
